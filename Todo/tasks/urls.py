79d2ea11c3f50d3e34445b958877f3473df930a5
Weten-Ogen <alphaoware@gmail.com>
2022-12-07 11:23:25 +0000

    stop at  a back with my update_task views

--- Todo/tasks/urls.py
+++ Todo/tasks/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name='task'
 urlpatterns = [ 
-    path('',views.index ,name='index'),
+    path('',views.index ,name='list'),
+    path('update_task/<int:task_id>', views.updateTask, name='update_task'),
 ]
--- Todo/tasks/views.py
+++ Todo/tasks/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render,redirect
-from .models import *
-from .forms import * 
+from .models import Task
+from .forms import TaskForm 
 # Create your views here.
+
 def index(request):
+    # Task Models and the Form entry
     tasks = Task.objects.all()
     form = TaskForm()
-
+    # Redirect the user to the same page
     if request.method == "POST":
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
+        # After submitting a new task and saving
         return redirect('/')
-
-
     context = {'tasks': tasks, 'form': form}
     return render(request, 'tasks/index.html',context)
+
+def updateTask(request, task_id):
+    task = Task.objects.GET(id=task_id)
+    form = TaskForm(instance=task)
+    context = {'form' : form}
+    return (request,'tasks/update_task.html', context)
