97cd857bdcf8bc1a88b3597cf43ca5958c0001e4
Weten-Ogen <alphaoware@gmail.com>
2024-09-03 18:11:57 +0000

    implemented the migrate.ts + authentication

--- components/typography/small.tsx
+++ components/typography/small.tsx
@@ -1,6 +1,8 @@
 import { cn } from "@/lib/utils";
+import { typo } from "@/types/next";
 
-export function TypographySmall({content,className}:typo) {
+export function TypographySmall({content,className}:typo
+) {
     return (
       <small className={cn("text-sm font-medium leading-none",className)}>
         {content}
--- components/typography/typoList.tsx
+++ components/typography/typoList.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils"
+import { typolistprops } from "@/types/next"
 
 export function TypographyList({content,className,listClass}:typolistprops) {
     return (
