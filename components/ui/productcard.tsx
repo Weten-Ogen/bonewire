f2b25ddfa8931e739427931f05484565e8d744ef
Weten-Ogen <alphaoware@gmail.com>
2024-09-04 17:16:05 +0000

    implemented the file uplaod from pages/file

--- components/ui/productcard.tsx
+++ components/ui/productcard.tsx
@@ -9,7 +9,7 @@ const ProductCard = ({content,gender,price,id,imageurl,label,tags,className}:pro
   return (
     <div className='p-2'>
       <Card className='p-0 '>
-        <CardContent className='flex flex-col  gap-4 p-0 rounded-lg'>
+        <CardContent className='flex flex-col  gap-2 p-0 rounded-lg '>
           <Image 
           src={imageurl}
           alt={label}
@@ -17,25 +17,27 @@ const ProductCard = ({content,gender,price,id,imageurl,label,tags,className}:pro
           height={200}
           className='w-full rounded-lg aspect-square'
           />
-          <CardTitle className='text-lg p-2'>
-             {label}
-          </CardTitle>
-          <CardDescription className='p-2'>
-             {content}
-          </CardDescription>
-          <div className='flex flex-col gap-4 rounded-lg p-2'>
-            <div  className='flex items-center justify-between gap-4 '>
-              <p className='font-bold'>Name</p>
-              <p className='font-medium leading-tight text-sm'>{label}</p>
-            </div>
-            <div className='flex items-center justify-between gap-4 '>
-            <p className='font-bold'>Price</p>
-            <p className='font-medium leading-tight text-sm'>{price}</p>
-            </div>
-            <div className='flex items-center justify-between gap-4 '>
-            <p className='font-bold'>Gender</p>
-            <p className='font-medium leading-tight text-sm'>{gender}</p>
-            </div>
+          <div className='p-2 flex flex-col gap-3'>
+            <CardTitle className='text-lg '>
+              {label}
+            </CardTitle>
+            <CardDescription className='px-1'>
+              {content}
+            </CardDescription>
+            <div className='flex flex-col gap-4 rounded-lg '>
+              <div  className='flex items-center justify-between gap-4 '>
+                <p className='font-bold'>Name</p>
+                <p className='font-medium leading-tight text-sm'>{label}</p>
+              </div>
+              <div className='flex items-center justify-between gap-4 '>
+              <p className='font-bold'>Price</p>
+              <p className='font-medium leading-tight text-sm'>{price}</p>
+              </div>
+              <div className='flex items-center justify-between gap-4 '>
+              <p className='font-bold'>Gender</p>
+              <p className='font-medium leading-tight text-sm'>{gender}</p>
+              </div>
+          </div>
 
             
           </div>
