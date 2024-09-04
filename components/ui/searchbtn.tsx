ec03bbbba8c37f62915a5125a1e5d553ca66567f
Weten-Ogen <alphaoware@gmail.com>
2024-09-04 15:44:48 +0000

    implemented the file uplaod from pages/file

--- components/ui/searchbtn.tsx
+++ components/ui/searchbtn.tsx
@@ -10,7 +10,7 @@ const SearchBtn = ({className}:searchbtnprops) => {
   return (
     <div className={cn('',className)}>
       <Input 
-      className='py-8 border border-yellow-500 text-md  hover:ring-black md:py-6  md:px-4 w-full '
+      className='py-4 border border-yellow-500 text-md  hover:ring-black md:py-6  md:px-4 w-full '
       placeholder='search for any kente product ....'
       type="text"/>
       <Button className='text-lg mt-4 w-full md:w-[25%] md:mt-0 py-8 px-2 md:py-6  md:px-4 touchbtn flex items-center gap-4' type="submit">
