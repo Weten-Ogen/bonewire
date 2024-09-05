ad1bf47a361acb73086514423c08cabe18cb55a0
Weten-Ogen <alphaoware@gmail.com>
2024-09-05 10:09:01 +0000

    implemented the file uplaod from pages/file

--- components/ui/productcard.tsx
+++ components/ui/productcard.tsx
@@ -13,7 +13,7 @@ const ProductCard = ({content,gender,price,id,imageurl,label,tags,className}:pro
       <Card
       
        className='p-0   object-contain rounded-lg outline-none bg-transparent  '>
-        <CardContent className='flex flex-col object-contain  gap-2 p-0 relative bg-transparent m-0   rounded-lg outline-none shadow-lg'>
+        <CardContent className='flex flex-col object-contain  gap-2 p-0 relative bg-transparent m-0   outline-none shadow-lg'>
           <div className='w-full h-[200px] oveflow-hidden object-cotain'>
 
           <Image 
