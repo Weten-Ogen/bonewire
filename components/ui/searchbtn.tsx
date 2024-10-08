5da2d09f237de2c2e99786ff33c11ed3ac9eddc1
Weten-Ogen <alphaoware@gmail.com>
2024-10-08 13:22:51 +0000

    first commit

--- components/ui/searchbtn.tsx
+++ components/ui/searchbtn.tsx
@@ -7,15 +7,13 @@ import { Button } from './button'
 const SearchBtn = () => {
   return (
     <>
-    
     <div className='flex items-center  gap-2 m-2'>
       <Input 
        placeholder='search for all products here ...'      
        />
       <Button  className='w-40'>Search</Button>
     </div>
-    </div>
-       </>
+    </>
   )
 }
 
