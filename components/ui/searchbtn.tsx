f47fdc84a06c6ca232192c1f3ad1b6bf52722e14
Weten-Ogen <alphaoware@gmail.com>
2024-10-08 13:08:13 +0000

    first commit

--- components/ui/searchbtn.tsx
+++ components/ui/searchbtn.tsx
@@ -2,24 +2,25 @@
 import React from 'react'
 import { Input } from './input'
 import { Button } from './button'
-import TypographyBlockquote from '@/components/typography/blockquote'
 
 
 const SearchBtn = () => {
   return (
-    <div>
-    <TypographyBlockquote className='text-xl py-4'>
-        Buy all your product here,
-        Feel free to add to Cart here.
-        All products are made just for.
-    </TypographyBlockquote>
+    <>
+    // <div>
+    // <TypographyBlockquote className='text-xl py-4'>
+    //     Buy all your product here,
+    //     Feel free to add to Cart here.
+    //     All products are made just for.
+    // </TypographyBlockquote>
     <div className='flex items-center  gap-2 m-2'>
       <Input 
        placeholder='search for all products here ...'      
-      />
+       />
       <Button  className='w-40'>Search</Button>
     </div>
     </div>
+       </>
   )
 }
 
