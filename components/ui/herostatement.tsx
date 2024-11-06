cb4e6afa1ccdc3094e59633dc6d9ae143e910ab1
Weten-Ogen <alphaoware@gmail.com>
2024-11-06 10:47:57 +0000

    added the the hero statement

--- components/ui/herostatement.tsx
+++ components/ui/herostatement.tsx
@@ -9,7 +9,7 @@ interface heroprops {
 const HeroStatement = (props:heroprops) => {
   return (
     <section id="productsection" className={cn('',props.className)}>
-      <TypographyLarge className='tracking-wider max-w-4xl text-lg'>
+      <TypographyLarge className='text-center tracking-wider max-w-4xl  text-md md:text-lg'>
         Bonewire is the Leading Ecommerce platform that makes you experience Ghana through culture.All products are hand-made the african way. Africans take pride in their culture because of the blood and tears invested in the process. 
         
         We aim to make everyone experience the true BlackStar spirit of an African. Our Ancestors laid their blood for the freedom to experience our culuture.
