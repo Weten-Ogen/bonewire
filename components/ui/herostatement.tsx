ec6f85ad4cd86b88f8a4ae24a7d40d9934025739
Weten-Ogen <alphaoware@gmail.com>
2024-11-10 02:23:57 +0000

    edited the herostatement and also styled the herocarousel and herocaroucomp

--- components/ui/herostatement.tsx
+++ components/ui/herostatement.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react'
 import { cn } from '@/lib/utils'
-import { TypographyBlockquote } from '../typography/blockquote'
+import { TypographyBlockquote } from '@/components/typography/blockquote'
 
 
 interface heroprops {
@@ -10,7 +10,7 @@ interface heroprops {
 const HeroStatement = (props:heroprops) => {
   return (
     <section id="productsection" className={cn('',props.className)}>
-      <TypographyBlockquote className='  text-md md:text-lg'>
+      <TypographyBlockquote className='text-md md:text-lg'>
         Bonewire is the Leading Ecommerce platform that makes you experience Ghana through culture.All products are hand-made the african way. Africans take pride in their culture because of the blood and tears invested in the process. 
         We aim to make everyone experience the true BlackStar spirit of an African. Our Ancestors laid their blood for the freedom to experience our culuture.
         Do not miss out being an African, African, is not where you are born , is who you are and never look down on that.
