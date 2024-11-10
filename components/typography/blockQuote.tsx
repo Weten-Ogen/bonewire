f32cd1210ced16156b2446c66593417fad1c6804
Weten-Ogen <alphaoware@gmail.com>
2024-11-10 02:31:22 +0000

    fixed the blockquote cannot be found

--- components/typography/blockQuote.tsx
+++ components/typography/blockQuote.tsx
@@ -1,13 +1,12 @@
-"use client"
 import { cn } from "@/lib/utils";
 import React from "react";
-
-
-interface TypographyBlockquoteprops {
-    className?: string,
-    children : string
+interface comprops {
+  className?:string,
+  children:string
 }
-export function TypographyBlockquote(props:TypographyBlockquoteprops) {
+
+
+export function TypographyBlockquote(props:comprops) {
   return (
     <blockquote className={cn("mt-6 border-l-2 pl-6 italic",props.className)}>
       {props.children}
