0f29c733f81fda9743c2103a234cb7091f09f76d
Weten-Ogen <alphaoware@gmail.com>
2024-11-19 17:43:20 +0000

    fixed the product at id page , now working like it should

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -5,14 +5,13 @@ import Image from 'next/image';
 import { TypographyLarge } from '@/components/typography/small';
 import { TypographyLead } from '@/components/typography/lead';
 import { getproductsbyid } from '../actions/fetch';
-import ProfileCard from './profilecardt';
+
 
 const Profile = () => {
 
   return (
     <div>
-      <ProfileCard
-      />
+      
     </div>
   )
 }
