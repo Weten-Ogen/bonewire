131acafffad2bc95b86967a1e73105bd9456c15f
Weten-Ogen <alphaoware@gmail.com>
2024-11-22 01:52:19 +0000

    fixed the cart page data error

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,10 +1,6 @@
-import CoverProfile from '@/components/sections/coverprofile';
 import React from 'react'
-import { getUserInfo } from '../actions/authservice';
 import Image from 'next/image';
-import { TypographyLarge } from '@/components/typography/small';
-import { TypographyLead } from '@/components/typography/lead';
-import { getproductsbyid } from '../actions/fetch';
+
 
 
 const Profile = () => {
