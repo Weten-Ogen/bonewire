746ce5cb75e5e4bd3b944cedb5923121d25a232d
Weten-Ogen <alphaoware@gmail.com>
2024-11-22 01:51:40 +0000

    fixed the cart page data error

--- app/(root)/cart/page.tsx
+++ app/(root)/cart/page.tsx
@@ -1,6 +1,3 @@
-import CartGrid from '@/components/sections/cartgrid'
-import { TypographyLarge } from '@/components/typography/large'
-import { fakedata } from '@/lib/constants'
 import React from 'react'
 
 
@@ -8,18 +5,7 @@ const Cart = () => {
   
   return (
     <section className='mt-20  p-4 md:p-6'>
-     {
-      !fakedata && <TypographyLarge className="">
-        You have no item in the cart
-      </TypographyLarge>
-     }
-     {
-      fakedata &&
-     <CartGrid
-     data={fakedata}
-     className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-4 items-center'
-     /> 
-     }
+    
     </section>
   )
 }
