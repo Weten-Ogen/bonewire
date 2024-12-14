0c0db39abbc2dae182074978c3ec328ad287bd8b
Weten-Ogen <alphaoware@gmail.com>
2024-12-14 02:03:12 +0000

    removed the svg from the application

--- components/ui/heroinfo.tsx
+++ components/ui/heroinfo.tsx
@@ -14,8 +14,8 @@ const HeroInfo = (props:heroinfoprops) => {
     <section 
     id="heroinfo" 
     className={cn('grid grid-cols-1 md:grid-cols-3 w-[99%] mx-auto item-center justify-between h-[140vh] mt-5 md:h-[60vh] p-4 gap-8 md:gap-12 ',props.className)}>
-      <div className='pt-0 md:pt-10 relative border border-gray-200 p-4 rounded-lg shadow-lg flex flex-col items-start'>
-        <div className="w-0.5 h-10 -z-10 top-0 left-8 absolute bg-yellow-300 "/>
+      <div className='pt-0 md:pt-10 relative  p-4 rounded-lg shadow-xl flex flex-col items-start'>
+       
         <Truck className="bg-yellow-300 rounded-lg p-2 " size={56}/>
         <div className=''>
             <TypographyH4 className='text-2xl pt-4'>Global Delivery</TypographyH4>
@@ -23,8 +23,8 @@ const HeroInfo = (props:heroinfoprops) => {
             We aid you finding a delivery agent here, that matches yor budget </TypographyP>
         </div>
       </div >
-      <div className='pt-0 md:pt-10  border border-gray-200 p-4 rounded-lg shadow-lg flex flex-col relative items-start'>
-      <div className="w-0.5 h-10 -z-10 top-0 left-8 absolute bg-yellow-300 "/>
+      <div className='pt-0 md:pt-10  border  p-4 rounded-lg shadow-xl flex flex-col relative items-start'>
+    
         <Server 
         className="bg-yellow-300 rounded-lg p-2 "
          size={56}/>
@@ -34,8 +34,8 @@ const HeroInfo = (props:heroinfoprops) => {
                 We always here to aid you in your search for authentic cultural experience online.We think culture is for the royalties of which you are a part. We here to  serve you.
             </TypographyP>
       </div>
-      <div className='pt-0 md:pt-10  border relative border-gray-200 p-4 rounded-lg shadow-lg flex flex-col items-start'>
-      <div className="w-0.5 h-10 -z-10 top-0 left-10 absolute bg-yellow-300 "/>
+      <div className='pt-0 md:pt-10  border relative shadow-white p-4 rounded-lg shadow-lg flex flex-col items-start'>
+
         <HeartHandshake className="bg-yellow-300 rounded-lg p-2 " size={56}/>
     
             <TypographyH4 className='text-2xl pt-4'>
