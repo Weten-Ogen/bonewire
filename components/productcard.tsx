96b8c82e6754ae2c0e4a7d55eaaf66b208ef371b
Weten-Ogen <alphaoware@gmail.com>
2024-12-15 00:23:11 +0000

    added the profile and about page

--- components/productcard.tsx
+++ components/productcard.tsx
@@ -30,7 +30,7 @@ const ProductCard = (props:productcardprops) => {
     <div className={cn(' rounded-t-lg shadow-lg',props.className)}>
       <Card className="rounded-t-lg">
         <CardContent className='flex flex-col gap-4 items-start rounded-t-lg p-0'>
-            <div className="w-full h-[200px] overflow-hidden  rounded-t-lg">
+            <div className="w-full h-[180px] overflow-hidden  rounded-t-lg">
                 <Image 
                 className='object-cover rounded-t-lg  max-w-full h-auto'
                 src={props.imageUrl}
