7a48e52f175b8299cca49a88af33b04ccaf015c0
Weten-Ogen <alphaoware@gmail.com>
2025-01-14 16:49:50 +0000

    added the styles for the chat comp

--- app/(root)/chat/messageui.tsx
+++ app/(root)/chat/messageui.tsx
@@ -11,10 +11,10 @@ interface messageprops {
 const Message = (props:messageprops) => {   
     const avatar= "u"
   return (
-    <div className={` flex gap-2  p-2 
+    <div className={` flex gap-2  
     bg-transparent rounded-lg `}>
-      <p className='rounded-full font-extrabold font-sans uppercase bg-gray-200 p-4 '>{avatar}</p>
-      <p className='font-bold font-serif text-base text-left bg-gray-200  p-4 rounded-lg'>{props.message}</p>
+      <p className='rounded-full font-extrabold font-sans uppercase bg-gray-200 p-2 '>{avatar}</p>
+      <p className='font-bold font-serif text-sm text-left bg-gray-200  p-2 rounded-lg'>{props.message}</p>
     </div>
   )
 }
