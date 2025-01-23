c72c969b30a1968e6c30fcc8be1ba1269e535a57
Weten-Ogen <alphaoware@gmail.com>
2025-01-23 14:16:04 +0000

    added the api for chat

--- app/actions/mesage.ts
+++ app/actions/mesage.ts
@@ -1,18 +1,12 @@
 "use server"
 
 import { pusherServer } from "@/lib/pusher"
-import Router from "next/router"
 
-export const createRoom = async(roomId: string) => {
-    const router = Router
-    const room = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/chat/createroom`)
-    router.push(`/chat/${roomId}`)
-}
+
 
 export const sendMessage = (message:string, roomId:string) => {    
-
     // trigger event
-    pusherServer.trigger(roomId,"incomming-message", 
+    pusherServer.trigger(roomId, "incomming-message", 
     {
         message
     }
