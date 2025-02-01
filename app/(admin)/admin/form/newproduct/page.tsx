601fd335bf25e808583cd0f1d92f3bb7beb8c421
Weten-Ogen <alphaoware@gmail.com>
2025-02-01 22:21:17 +0000

    styled the home video

--- app/(admin)/admin/form/newproduct/page.tsx
+++ app/(admin)/admin/form/newproduct/page.tsx
@@ -1,7 +1,7 @@
 import ProductForm from '@/app/forms/productform'
 import { TypographyH2 } from '@/components/typography/h2'
 
-export default function  page {
+export default function  page() {
   return (
     <section className=' flex flex-col  md:items-center p-8 md:p-12'>
       <TypographyH2 className='mb-5'>
