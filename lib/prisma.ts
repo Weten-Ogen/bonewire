cf03b0bb1ce0b4dfbf54eb18d80707cd212d4863
Weten-Ogen <alphaoware@gmail.com>
2025-04-28 08:46:53 +0000

    added the product login

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,4 @@
-import { PrismaClient } from '@/lib/generated/prisma';
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+export const prisma = new PrismaClient().$extends(withAccelerate())
